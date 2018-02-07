a3f2371461795ac1967bf4072ae33edf991d8d0f
Marvin Löbel <loebel.marvin@gmail.com>
2018-02-07 23:26:51 +0100

    import files

---
+++ dynamic_t.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <tudocomp/util/IntegerBase.hpp>
+
+#include <cstdint>
+
+namespace tdc {
+
+/// This exists to support the `GenericIntVector<dynamic_t>` specialization.
+class dynamic_t {};
+
+}
+
