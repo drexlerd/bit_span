18eab23721f1510aabb48daaeb7632b25217e282
Marvin Löbel <loebel.marvin@gmail.com>
2018-02-20 15:38:44 +0100

    Add (int)-pointer access to bit layout positions

--- include/tudocomp/util/bit_packed_layout_t.hpp
+++ include/tudocomp/util/bit_packed_layout_t.hpp
@@ -1,20 +1,77 @@
 #pragma once
 
 #include "maybe_bit_packed.hpp"
+#include <tudocomp/ds/IntVector.hpp>
+#include <tudocomp/util/metaprogramming.hpp>
+#include <tudocomp/ds/IntRepr.hpp>
 
-namespace tdc {
+namespace tdc {namespace int_vector {
 
 class bit_layout_element_t {
     uint64_t m_bit_offset = 0;
     uint64_t m_bit_size = 0;
+    uint64_t m_bit_element_size = 0;
 
 public:
-    constexpr bit_layout_element_t(uint64_t bit_offset, uint64_t bit_size):
+    constexpr bit_layout_element_t(uint64_t bit_offset, uint64_t bit_size, uint64_t element_size):
         m_bit_offset(bit_offset),
-        m_bit_size(bit_size) {}
+        m_bit_size(bit_size),
+        m_bit_element_size(element_size) {}
 
     constexpr uint64_t bit_offset() const { return m_bit_offset; }
     constexpr uint64_t bit_size() const { return m_bit_size; }
+    constexpr uint64_t bit_element_size() const { return m_bit_element_size; }
+};
+
+/// TODO: This is temporary and can be removed if the int vector implementations
+/// get more general
+template<typename T, typename X = tdc::void_t<>>
+struct maybe_bit_packed_pointer_trait_t {
+    using pointer_t = T*;
+
+    static constexpr pointer_t construct_relative_to(uint64_t* base_ptr,
+                                                     uint64_t bit_offset,
+                                                     uint64_t bit_element_size) {
+        DCHECK_EQ(bit_offset % 8, 0);
+        DCHECK_EQ(bit_element_size % 8, 0);
+        auto offset = bit_offset / 8;
+        return reinterpret_cast<T*>(reinterpret_cast<char*>(base_ptr) + offset);
+    }
+};
+
+template<typename T>
+struct maybe_bit_packed_pointer_trait_t<T, tdc::void_t<typename IntRepr<T>::IntPtrBase>> {
+    using pointer_t = IntPtr<T>;
+
+    using ptr_base_t = IntPtrBase<pointer_t>;
+
+    static constexpr pointer_t construct_relative_to(uint64_t* base_ptr,
+                                                     uint64_t bit_offset,
+                                                     uint64_t bit_element_size) {
+        // Find the uint64_t in which the int pointer starts
+        base_ptr += bit_offset / 64;
+        bit_offset = bit_offset % 64;
+
+        auto int_ptr_base = ptr_base_t(base_ptr, bit_offset, bit_element_size);
+        auto int_ptr = pointer_t(int_ptr_base);
+
+        return int_ptr;
+    }
+
+};
+
+template<typename T>
+class maybe_bit_packed_layout_element_t: public bit_layout_element_t {
+public:
+    using pointer_t = typename maybe_bit_packed_pointer_trait_t<T>::pointer_t;
+
+    constexpr pointer_t ptr_relative_to(uint64_t* base_ptr) const {
+        return maybe_bit_packed_pointer_trait_t<T>::construct_relative_to(base_ptr, bit_offset(), bit_element_size());
+    }
+
+    using bit_layout_element_t::bit_layout_element_t;
+    constexpr maybe_bit_packed_layout_element_t(bit_layout_element_t&& base):
+        bit_layout_element_t(std::move(base)) {}
 };
 
 
@@ -38,7 +95,7 @@ public:
 
         m_bit_offset += bit_size;
 
-        return bit_layout_element_t(bit_offset, bit_size);
+        return bit_layout_element_t(bit_offset, bit_size, 8ull * byte_size);
     }
 
     constexpr bit_layout_element_t bit_packed_elements(size_t bit_size, size_t n) {
@@ -46,16 +103,16 @@ public:
         uint64_t rbit_size = bit_size * n;
         m_bit_offset += rbit_size;
 
-        return bit_layout_element_t(rbit_offset, rbit_size);
+        return bit_layout_element_t(rbit_offset, rbit_size, bit_size);
     }
 
     template<typename T>
-    constexpr bit_layout_element_t aligned_elements(size_t n) {
+    constexpr maybe_bit_packed_layout_element_t<T> aligned_elements(size_t n) {
         return aligned_elements(alignof(T), sizeof(T), n);
     }
 
     template<typename T>
-    constexpr bit_layout_element_t maybe_bit_packed_elements(size_t n, maybe_bit_packed_width_t<T> const& meta) {
+    constexpr maybe_bit_packed_layout_element_t<T> maybe_bit_packed_elements(size_t n, maybe_bit_packed_width_t<T> const& meta) {
         if(meta.needs_alignment()) {
             return aligned_elements<T>(n);
         } else {
@@ -70,4 +127,4 @@ public:
 
 };
 
-}
+}}
--- test/bit_packed_layout_tests.cpp
+++ test/bit_packed_layout_tests.cpp
@@ -9,6 +9,7 @@
 #include <tudocomp/util/bit_packed_layout_t.hpp>
 
 using namespace tdc;
+using namespace int_vector;
 
 TEST(bit_layout, test1) {
     auto layout = bit_layout_t();
@@ -33,6 +34,14 @@ TEST(bit_layout, test1) {
     ASSERT_EQ(quot_bv.bit_size(), 90);
 
     ASSERT_EQ(layout.get_size_in_uint64_t_units(), 5);
+
+    std::array<uint64_t, 5> m_data;
+    auto byte_offset = [&](auto ptr) {
+        return ((char*) ptr) - ((char*) m_data.data());
+    };
+
+    auto val_ptr = values.ptr_relative_to(m_data.data());
+    ASSERT_EQ(byte_offset(val_ptr), 8);
 }
 
 TEST(bit_layout, test2) {
@@ -57,6 +66,14 @@ TEST(bit_layout, test2) {
     ASSERT_EQ(quot_bv.bit_size(), 90);
 
     ASSERT_EQ(layout.get_size_in_uint64_t_units(), 4);
+
+    std::array<uint64_t, 4> m_data;
+    auto byte_offset = [&](auto ptr) {
+        return ((char*) ptr) - ((char*) m_data.data());
+    };
+
+    auto val_ptr = values.ptr_relative_to(m_data.data());
+    ASSERT_EQ(byte_offset(val_ptr), 8);
 }
 
 TEST(bit_layout, test3) {
@@ -81,6 +98,8 @@ TEST(bit_layout, test3) {
     ASSERT_EQ(quot_bv.bit_size(), 45);
 
     ASSERT_EQ(layout.get_size_in_uint64_t_units(), 3);
+
+    // std::array<uint64_t, 3> m_data;
 }
 
 TEST(bit_layout, test4) {
@@ -109,6 +128,19 @@ TEST(bit_layout, test4) {
     ASSERT_EQ(quot_bv.bit_size(), 45);
 
     ASSERT_EQ(layout.get_size_in_uint64_t_units(), 3);
+
+    std::array<uint64_t, 3> m_data;
+    auto byte_offset = [&](auto ptr) {
+        return ((char*) ptr) - ((char*) m_data.data());
+    };
+
+    auto val_ptr = values.ptr_relative_to(m_data.data());
+    ASSERT_EQ(byte_offset(val_ptr.internal_ptr()), 8);
+    ASSERT_EQ(val_ptr.internal_bit_offset(), 0);
+
+    auto quot_ptr = quot_bv.ptr_relative_to(m_data.data());
+    ASSERT_EQ(byte_offset(quot_ptr.internal_ptr()), 8);
+    ASSERT_EQ(quot_ptr.internal_bit_offset(), 45);
 }
 
 TEST(bit_layout, test5) {
@@ -122,10 +154,10 @@ TEST(bit_layout, test5) {
     auto quot_width = maybe_bit_packed_width_t<dynamic_t>(30);
     auto quot_bv = layout.maybe_bit_packed_elements<dynamic_t>(3, quot_width);
 
-    // [        |        |        ]
+    // [        |        |        |        ]
     // [   bv   ]
     //          [values]
-    //                 [ quot ]
+    //                 [      quot    ]
 
     ASSERT_EQ(bv.bit_offset(), 0);
     ASSERT_EQ(bv.bit_size(), 64);
@@ -137,4 +169,95 @@ TEST(bit_layout, test5) {
     ASSERT_EQ(quot_bv.bit_size(), 3 * 30);
 
     ASSERT_EQ(layout.get_size_in_uint64_t_units(), 4);
+
+    std::array<uint64_t, 4> m_data;
+    auto byte_offset = [&](auto ptr) {
+        return ((char*) ptr) - ((char*) m_data.data());
+    };
+
+    auto val_ptr = values.ptr_relative_to(m_data.data());
+    ASSERT_EQ(byte_offset(val_ptr.internal_ptr()), 8);
+    ASSERT_EQ(val_ptr.internal_bit_offset(), 0);
+
+    auto quot_ptr = quot_bv.ptr_relative_to(m_data.data());
+    ASSERT_EQ(byte_offset(quot_ptr.internal_ptr()), 8);
+    ASSERT_EQ(quot_ptr.internal_bit_offset(), 45);
+}
+
+TEST(bit_layout, test6) {
+    auto layout = bit_layout_t();
+
+    auto bv = layout.aligned_elements<uint64_t>(1);
+
+    auto values = layout.aligned_elements<uint32_t>(3);
+    layout.aligned_elements<uint64_t>(0);
+    auto quot_width = maybe_bit_packed_width_t<dynamic_t>(30);
+    auto quot_bv = layout.maybe_bit_packed_elements<dynamic_t>(3, quot_width);
+
+    // [        |        |        |        |        ]
+    // [   bv   ]
+    //          [   values    ]
+    //                            [    quot   ]
+
+    ASSERT_EQ(bv.bit_offset(), 0);
+    ASSERT_EQ(bv.bit_size(), 64);
+
+    ASSERT_EQ(values.bit_offset(), 64);
+    ASSERT_EQ(values.bit_size(), 3 * 32);
+
+    ASSERT_EQ(quot_bv.bit_offset(), 64 * 3);
+    ASSERT_EQ(quot_bv.bit_size(), 90);
+
+    ASSERT_EQ(layout.get_size_in_uint64_t_units(), 5);
+
+    std::array<uint64_t, 5> m_data;
+    auto byte_offset = [&](auto ptr) {
+        return ((char*) ptr) - ((char*) m_data.data());
+    };
+
+    auto val_ptr = values.ptr_relative_to(m_data.data());
+    ASSERT_EQ(byte_offset(val_ptr), 8);
+
+    auto quot_ptr = quot_bv.ptr_relative_to(m_data.data());
+    ASSERT_EQ(byte_offset(quot_ptr.internal_ptr()), 24);
+    ASSERT_EQ(quot_ptr.internal_bit_offset(), 0);
+}
+
+TEST(bit_layout, test7) {
+    auto layout = bit_layout_t();
+
+    auto bv = layout.aligned_elements<uint64_t>(1);
+
+    auto values = layout.aligned_elements<uint32_t>(3);
+    auto quot_width = maybe_bit_packed_width_t<dynamic_t>(30);
+    auto quot_bv = layout.maybe_bit_packed_elements<dynamic_t>(3, quot_width);
+
+
+    // [        |        |        |        ]
+    // [   bv   ]
+    //          [   values    ]
+    //                        [    quot   ]
+
+    ASSERT_EQ(bv.bit_offset(), 0);
+    ASSERT_EQ(bv.bit_size(), 64);
+
+    ASSERT_EQ(values.bit_offset(), 64);
+    ASSERT_EQ(values.bit_size(), 3 * 32);
+
+    ASSERT_EQ(quot_bv.bit_offset(), 64 + 3 * 32);
+    ASSERT_EQ(quot_bv.bit_size(), 90);
+
+    ASSERT_EQ(layout.get_size_in_uint64_t_units(), 4);
+
+    std::array<uint64_t, 4> m_data;
+    auto byte_offset = [&](auto ptr) {
+        return ((char*) ptr) - ((char*) m_data.data());
+    };
+
+    auto val_ptr = values.ptr_relative_to(m_data.data());
+    ASSERT_EQ(byte_offset(val_ptr), 8);
+
+    auto quot_ptr = quot_bv.ptr_relative_to(m_data.data());
+    ASSERT_EQ(byte_offset(quot_ptr.internal_ptr()), 16);
+    ASSERT_EQ(quot_ptr.internal_bit_offset(), 32);
 }
